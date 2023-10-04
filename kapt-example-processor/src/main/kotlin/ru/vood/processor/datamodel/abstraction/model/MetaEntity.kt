010265b44a78a6813275221ec5325e16fe83c0f1
vood <balitsky_igor@list.ru>
2023-10-04 11:31:38 +0300

    init

--- kapt-example-processor/src/main/kotlin/ru/vood/processor/datamodel/abstraction/model/MetaEntity.kt
+++ kapt-example-processor/src/main/kotlin/ru/vood/processor/datamodel/abstraction/model/MetaEntity.kt
@@ -2,6 +2,7 @@ package ru.vood.processor.datamodel.abstraction.model
 
 import ru.vood.dmgen.annotation.FlowEntity
 import ru.vood.dmgen.annotation.ForeignKey
+import ru.vood.dmgen.annotation.Uk
 import ru.vood.processor.datamodel.abstraction.model.abstraction.AbstractAnnotatedClass
 import ru.vood.processor.datamodel.abstraction.model.abstraction.annotation
 import ru.vood.processor.datamodel.abstraction.model.abstraction.annotations
@@ -15,4 +16,6 @@ class MetaEntity(element: Element): AbstractAnnotatedClass<MetaEntityColumn>(ele
     val flowEntity by lazy { element.annotation<FlowEntity>() }
 
     val foreignKeys by lazy { element.annotations<ForeignKey>() }
+
+    val uniqueKeys by lazy { element.annotations<Uk>() }
 }
