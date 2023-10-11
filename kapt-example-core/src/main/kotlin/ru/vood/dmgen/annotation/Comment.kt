ab7095356e72203f63e00a2b0d5dc4a365b5a93d
vood <balitsky_igor@list.ru>
2023-10-11 20:11:41 +0300

    init

--- kapt-example-core/src/main/kotlin/ru/vood/dmgen/annotation/Comment.kt
+++ kapt-example-core/src/main/kotlin/ru/vood/dmgen/annotation/Comment.kt
@@ -2,18 +2,8 @@ package ru.vood.dmgen.annotation
 
 @Target(
     AnnotationTarget.CLASS,
-    AnnotationTarget.FIELD,
-    AnnotationTarget.VALUE_PARAMETER,
-    AnnotationTarget.FIELD,
-    AnnotationTarget.FUNCTION,
     AnnotationTarget.PROPERTY,
-    AnnotationTarget.LOCAL_VARIABLE,
-    AnnotationTarget.PROPERTY_GETTER,
-    AnnotationTarget.PROPERTY_SETTER,
-    AnnotationTarget.FUNCTION,
-    AnnotationTarget.CONSTRUCTOR,
-    AnnotationTarget.TYPE,
-//    AnnotationTarget.EXPRESSION,
+    AnnotationTarget.VALUE_PARAMETER,
 )
 @Retention(AnnotationRetention.RUNTIME)
 annotation class Comment(
