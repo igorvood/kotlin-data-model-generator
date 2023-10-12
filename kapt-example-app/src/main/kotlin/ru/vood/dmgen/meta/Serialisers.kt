25d515cbfb2f932c23ec9ae6fb84df53818de451
vood <balitsky_igor@list.ru>
2023-10-12 20:54:13 +0300

    init

--- kapt-example-app/src/main/kotlin/ru/vood/dmgen/meta/Serialisers.kt
+++ kapt-example-app/src/main/kotlin/ru/vood/dmgen/meta/Serialisers.kt
@@ -4,28 +4,22 @@ import kotlinx.serialization.KSerializer
 import ru.vood.dmgen.datamodel.metaEnum.DataDictionaryEntityEnum
 import ru.vood.dmgen.intf.IEntity
 import ru.vood.spring.fabric.JsonSerializer
+import kotlin.reflect.KClass
 
 
-val entitySerializers by lazy {
+val entitySerializers: Map<KClass<out IEntity<*>>, KSerializer<out IEntity<*>>> =
     DataDictionaryEntityEnum.values().associate { it.runtimeClass to it.serializer }
-}
 
-
-inline fun <reified T : IEntity<T>> toDto(data: String): IEntity<T> {
-    val kSerializer1 = entitySerializers[T::class]!!.let { it as KSerializer<IEntity<T>> }
+@Suppress("UNCHECKED_CAST")
+inline fun <reified T : IEntity<T>> toDto(data: String): IEntity<T> =
+    entitySerializers[T::class]!!.let { it as KSerializer<IEntity<T>> }
         .let { JsonSerializer.json.decodeFromString(it, data) }
 
 
-    return kSerializer1
-}
-
-inline fun <reified T : IEntity<T>> T.toJson(): String {
-    val kSerializer1 = entitySerializers[T::class]!!.let { it as KSerializer<IEntity<T>> }
+@Suppress("UNCHECKED_CAST")
+inline fun <reified T : IEntity<T>> T.toJson(): String =
+    entitySerializers[T::class]!!.let { it as KSerializer<IEntity<T>> }
         .let { JsonSerializer.json.encodeToString(it, this) }
-
-
-    return kSerializer1
-}
 
 
 
