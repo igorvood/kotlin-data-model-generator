e4c9177878bdf77f4da97067a5bae4adde35ae64
vood <balitsky_igor@list.ru>
2023-10-27 18:56:25 +0300

    init

--- kapt-example-processor/src/main/kotlin/ru/vood/processor/datamodel/abstraction/model/gen/UniqueKeyEnumGenerator.kt
+++ kapt-example-processor/src/main/kotlin/ru/vood/processor/datamodel/abstraction/model/gen/UniqueKeyEnumGenerator.kt
@@ -9,6 +9,7 @@ import ru.vood.processor.datamodel.abstraction.model.gen.dto.FileName
 import ru.vood.processor.datamodel.abstraction.model.gen.dto.GeneratedCode
 import ru.vood.processor.datamodel.abstraction.model.gen.dto.GeneratedFile
 import ru.vood.processor.datamodel.abstraction.model.gen.dto.PackageName
+import ru.vood.processor.datamodel.abstraction.model.gen.runtime.dataclasses.ContextDataClassesGenerator.Companion.contextDataClassesGeneratorPackageName
 import javax.annotation.processing.Messager
 import javax.annotation.processing.ProcessingEnvironment
 import javax.tools.Diagnostic
@@ -39,7 +40,8 @@ class UniqueKeyEnumGenerator(
 
                                 """${ukDto.name.value}(
                                     |setOf($ukCols),
-                                    |${packageName.value}.${nameClassEntityEnumGenerator}.${metaEnt.shortName}
+                                    |${packageName.value}.${nameClassEntityEnumGenerator}.${metaEnt.shortName},
+                                    |${rootPackage.value}${contextDataClassesGeneratorPackageName.value}.${metaEnt.shortName}Context${ukDto.name.value}::class,
                                     |)""".trimMargin()
 
                             }
@@ -52,10 +54,12 @@ class UniqueKeyEnumGenerator(
                     """package ${packageName.value}
                         
 import ${packageName.value}.${ColumnEntityEnumGenerator.columnEntityEnumGeneratorNameClass}.*
+import kotlin.reflect.KClass
 
 enum class $nameClass(
     override val columns: Set<${IMetaColumnEntity::class.java.canonicalName}>,
-    override val entity : ${IMetaEntity::class.java.canonicalName}
+    override val entity : ${IMetaEntity::class.java.canonicalName},
+    override val contextOfClass: KClass<*>
 ): ${IMetaUkEntity::class.java.canonicalName}{
 $entities
 }
