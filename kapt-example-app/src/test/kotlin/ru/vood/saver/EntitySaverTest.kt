f9fde77208e8fbc46d46709c8cb81ed4ee09e008
vood <balitsky_igor@list.ru>
2023-10-27 20:13:54 +0300

    init

---
+++ kapt-example-app/src/test/kotlin/ru/vood/saver/EntitySaverTest.kt
@@ -0,0 +1,20 @@
+package ru.vood.saver
+
+import org.junit.jupiter.api.Test
+import ru.vood.dmgen.datamodel.runtime.dataclasses.DealParamOneToOneEntity
+
+internal class EntitySaverTest {
+
+    @Test
+    fun asd(){
+//        EntitySaver.save(DealEntity(
+//            1,
+//            DealParamOneToOneEntity(1,
+//            "asd",
+//                DealEntity()
+//            )
+//
+//        ))
+        val kProperty1 = DealParamOneToOneEntity::deal
+    }
+}
