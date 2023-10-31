9ef071308f7a849e385566ea2246274568799988
vood <balitsky_igor@list.ru>
2023-10-31 20:16:14 +0300

    init

--- kapt-example-app/src/main/kotlin/kaptexample/app/DataDictionaryEntityEnum.kt
+++ kapt-example-app/src/main/kotlin/kaptexample/app/DataDictionaryEntityEnum.kt
@@ -1,98 +1,113 @@
 package kaptexample.app
-import kotlin.reflect.KClass
+
 import kotlinx.serialization.KSerializer
-import ru.vood.dmgen.annotation.FlowEntityType.*
 import ru.vood.dmgen.annotation.FlowEntityType
+import ru.vood.dmgen.annotation.FlowEntityType.*
 import ru.vood.dmgen.datamodel.runtime.dataclasses.DealEntity
 import ru.vood.dmgen.intf.EntityName
 import ru.vood.dmgen.intf.IEntity
-import ru.vood.dmgen.intf.IMetaEntity
+import kotlin.reflect.KClass
 
 
-interface IMetaEntityNew {
-    val designClass: KClass<*>
-    val runtimeClass: KClass<*>
+interface IMetaEntityNew<T : IEntity<out T>> {
+    val designClass: KClass<out Any>
+    val runtimeClass: KClass<out T>
     val entityName: EntityName
     val comment: String
-    val serializer: KSerializer<*>
+    val serializer: KSerializer<out IEntity<out T>>
     val entityType: FlowEntityType
 
 }
 
-data class EntityData< T: IEntity<out T>>(
+data class EntityData<T : IEntity<out T>>(
     override val designClass: KClass<out Any>,
-    override val runtimeClass: KClass<ru.vood.dmgen.intf.IEntity<out T>>,
-    override val serializer: KSerializer<out ru.vood.dmgen.intf.IEntity<out T>>,
-    override val entityName: ru.vood.dmgen.intf.EntityName,
+    override val runtimeClass: KClass<out T>,
+    override val serializer: KSerializer<out IEntity<out T>>,
+    override val entityName: EntityName,
     override val comment: String,
     override val entityType: FlowEntityType
-): IMetaEntityNew
+) : IMetaEntityNew<T>
 
 
 //val runtimeClass1 = ru.vood.dmgen.datamodel.runtime.dataclasses.DealEntity::class as IEntity<out DealEntity>
-//val asdqqq: Map<ru.vood.dmgen.datamodel.metaEnum.DataDictionaryEntityEnum, EntityData<DealEntity>> = mapOf(ru.vood.dmgen.datamodel.metaEnum.DataDictionaryEntityEnum.Deal to EntityData(
-//    ru.vood.dmgen.datamodel.Deal::class,
-//    runtimeClass1,
-//    ru.vood.dmgen.datamodel.runtime.dataclasses.DealEntity.serializer(),
-//    ru.vood.dmgen.intf.EntityName("Deal"),
-//    "Это сущность Сделка",
-//    AGGREGATE
-//))
-
+val asdqqq = mapOf(
+    ru.vood.dmgen.datamodel.metaEnum.DataDictionaryEntityEnum.Deal to EntityData(
+        ru.vood.dmgen.datamodel.Deal::class,
+        ru.vood.dmgen.datamodel.runtime.dataclasses.DealEntity::class,
+        ru.vood.dmgen.datamodel.runtime.dataclasses.DealEntity.serializer(),
+        EntityName("Deal"),
+        "Это сущность Сделка",
+        AGGREGATE
+    ),
+    ru.vood.dmgen.datamodel.metaEnum.DataDictionaryEntityEnum.DealParamOneToOne to EntityData(
+        ru.vood.dmgen.datamodel.DealParamOneToOne::class,
+        ru.vood.dmgen.datamodel.runtime.dataclasses.DealParamOneToOneEntity::class,
+        ru.vood.dmgen.datamodel.runtime.dataclasses.DealParamOneToOneEntity.serializer(),
+        EntityName("DealParamOneToOne"),
+        "null",
+        INNER_MANDATORY
+    )
+)
 
 
 enum class DataDictionaryEntityEnum(
-override val designClass: KClass<out Any>,
-override val runtimeClass: KClass<out ru.vood.dmgen.intf.IEntity<*>>,
-override val serializer: KSerializer<out ru.vood.dmgen.intf.IEntity<*>>,
-override val entityName: ru.vood.dmgen.intf.EntityName,
-override val comment: String,
-override val entityType: FlowEntityType
+    override val designClass: KClass<out Any>,
+    override val runtimeClass: KClass<out ru.vood.dmgen.intf.IEntity<*>>,
+    override val serializer: KSerializer<out ru.vood.dmgen.intf.IEntity<*>>,
+    override val entityName: EntityName,
+    override val comment: String,
+    override val entityType: FlowEntityType
 
-): ru.vood.dmgen.intf.IMetaEntity {
-Deal(ru.vood.dmgen.datamodel.Deal::class, 
-ru.vood.dmgen.datamodel.runtime.dataclasses.DealEntity::class,
-ru.vood.dmgen.datamodel.runtime.dataclasses.DealEntity.serializer(),
-ru.vood.dmgen.intf.EntityName("Deal"), 
-"Это сущность Сделка",
-AGGREGATE
-),
-DealParamOneToOne(ru.vood.dmgen.datamodel.DealParamOneToOne::class, 
-ru.vood.dmgen.datamodel.runtime.dataclasses.DealParamOneToOneEntity::class,
-ru.vood.dmgen.datamodel.runtime.dataclasses.DealParamOneToOneEntity.serializer(),
-ru.vood.dmgen.intf.EntityName("DealParamOneToOne"), 
-"null",
-INNER_MANDATORY
-),
-DealParamOneToOneOptional(ru.vood.dmgen.datamodel.DealParamOneToOneOptional::class, 
-ru.vood.dmgen.datamodel.runtime.dataclasses.DealParamOneToOneOptionalEntity::class,
-ru.vood.dmgen.datamodel.runtime.dataclasses.DealParamOneToOneOptionalEntity.serializer(),
-ru.vood.dmgen.intf.EntityName("DealParamOneToOneOptional"), 
-"null",
-INNER_OPTIONAL
-),
-DealParamSet(ru.vood.dmgen.datamodel.DealParamSet::class, 
-ru.vood.dmgen.datamodel.runtime.dataclasses.DealParamSetEntity::class,
-ru.vood.dmgen.datamodel.runtime.dataclasses.DealParamSetEntity.serializer(),
-ru.vood.dmgen.intf.EntityName("DealParamSet"), 
-"null",
-INNER_OPTIONAL
-),
-Product(ru.vood.dmgen.datamodel.Product::class, 
-ru.vood.dmgen.datamodel.runtime.dataclasses.ProductEntity::class,
-ru.vood.dmgen.datamodel.runtime.dataclasses.ProductEntity.serializer(),
-ru.vood.dmgen.intf.EntityName("Product"), 
-"null",
-AGGREGATE
-),
-ProductPayments(ru.vood.dmgen.datamodel.ProductPayments::class, 
-ru.vood.dmgen.datamodel.runtime.dataclasses.ProductPaymentsEntity::class,
-ru.vood.dmgen.datamodel.runtime.dataclasses.ProductPaymentsEntity.serializer(),
-ru.vood.dmgen.intf.EntityName("ProductPayments"), 
-"null",
-AGGREGATE
-);
+) : ru.vood.dmgen.intf.IMetaEntity {
+    Deal(
+        ru.vood.dmgen.datamodel.Deal::class,
+        ru.vood.dmgen.datamodel.runtime.dataclasses.DealEntity::class,
+        ru.vood.dmgen.datamodel.runtime.dataclasses.DealEntity.serializer(),
+        EntityName("Deal"),
+        "Это сущность Сделка",
+        AGGREGATE
+    ),
+    DealParamOneToOne(
+        ru.vood.dmgen.datamodel.DealParamOneToOne::class,
+        ru.vood.dmgen.datamodel.runtime.dataclasses.DealParamOneToOneEntity::class,
+        ru.vood.dmgen.datamodel.runtime.dataclasses.DealParamOneToOneEntity.serializer(),
+        EntityName("DealParamOneToOne"),
+        "null",
+        INNER_MANDATORY
+    ),
+    DealParamOneToOneOptional(
+        ru.vood.dmgen.datamodel.DealParamOneToOneOptional::class,
+        ru.vood.dmgen.datamodel.runtime.dataclasses.DealParamOneToOneOptionalEntity::class,
+        ru.vood.dmgen.datamodel.runtime.dataclasses.DealParamOneToOneOptionalEntity.serializer(),
+        EntityName("DealParamOneToOneOptional"),
+        "null",
+        INNER_OPTIONAL
+    ),
+    DealParamSet(
+        ru.vood.dmgen.datamodel.DealParamSet::class,
+        ru.vood.dmgen.datamodel.runtime.dataclasses.DealParamSetEntity::class,
+        ru.vood.dmgen.datamodel.runtime.dataclasses.DealParamSetEntity.serializer(),
+        EntityName("DealParamSet"),
+        "null",
+        INNER_OPTIONAL
+    ),
+    Product(
+        ru.vood.dmgen.datamodel.Product::class,
+        ru.vood.dmgen.datamodel.runtime.dataclasses.ProductEntity::class,
+        ru.vood.dmgen.datamodel.runtime.dataclasses.ProductEntity.serializer(),
+        EntityName("Product"),
+        "null",
+        AGGREGATE
+    ),
+    ProductPayments(
+        ru.vood.dmgen.datamodel.ProductPayments::class,
+        ru.vood.dmgen.datamodel.runtime.dataclasses.ProductPaymentsEntity::class,
+        ru.vood.dmgen.datamodel.runtime.dataclasses.ProductPaymentsEntity.serializer(),
+        EntityName("ProductPayments"),
+        "null",
+        AGGREGATE
+    );
 
-@Suppress("UNCHECKED_CAST")
-inline fun <reified T> entitySerializer(): KSerializer<T> = this.serializer as KSerializer<T> 
+    @Suppress("UNCHECKED_CAST")
+    inline fun <reified T> entitySerializer(): KSerializer<T> = this.serializer as KSerializer<T>
 }
